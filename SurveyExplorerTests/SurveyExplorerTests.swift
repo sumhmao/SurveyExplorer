8aa54d13edb05183d35a75befb55baa06f2ee356
Chavalit Vanasapdamrong <fernanez.v@gmail.com>
2017-02-26 15:07:49 +0700

    Initial Commit

---
+++ SurveyExplorerTests/SurveyExplorerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SurveyExplorerTests.swift
+//  SurveyExplorerTests
+//
+//  Created by Chavalit Vanasapdamrong on 2/26/2560 BE.
+//  Copyright © 2560 Chavalit Vanasapdamrong. All rights reserved.
+//
+
+import XCTest
+@testable import SurveyExplorer
+
+class SurveyExplorerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
