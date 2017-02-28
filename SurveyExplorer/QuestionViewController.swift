ca234f6c80f41b5f5c2ca7d699a091e485135099
Chavalit Vanasapdamrong <fernanez.v@gmail.com>
2017-02-28 18:41:51 +0700

    navigation to new screen

--- SurveyExplorer/QuestionViewController.swift
+++ SurveyExplorer/QuestionViewController.swift
@@ -9,11 +9,30 @@
 import UIKit
 
 class QuestionViewController: BasedViewController {
-
-    var survey: Survey?
+    
+    var survey: Survey!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let _ = self.survey else {
+            _ = self.navigationController?.popViewController(animated: true)
+            return
+        }
+        
+        let backButton = UIBarButtonItem(image: UIImage(named: "back_white"), style: .plain, target: self, action: #selector(onBack))
+        backButton.tintColor = UIColor.white
+        self.navigationItem.leftBarButtonItem = backButton
+        
+        self.initializeData()
     }
-
+    
+    func onBack() {
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+    
+    func initializeData() {
+        self.title = self.survey.title
+    }
+    
 }
